864107a9cc0d9062ad5e8515730450494b020b45
BaeJinYeol <qowlsduf4488@gmail.com>
2023-09-14 20:51:08 +0900

    23.09.14 1018 🎉

---
+++ c++bits/1018.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int black(vector<vector<char>> board, int x, int y) {
+    int change = 0;
+    int i, k;
+    
+    for (i = x; i < x + 8; i = i + 2) {
+        for (k = y; k < y + 8; k = k + 2)
+            if (board[i][k] == 'W') change++;
+        for (k = y + 1; k < y + 8; k = k + 2)
+            if (board[i][k] == 'B') change++;
+    }
+    for (i = x + 1; i < x + 8; i = i + 2) {
+        for (k = y; k < y + 8; k = k + 2)
+            if (board[i][k] == 'B') change++;
+        for (k = y + 1; k < y + 8; k = k + 2)
+            if (board[i][k] == 'W') change++;
+    }
+
+    return change;
+}
+
+int white(vector<vector<char>> board, int x, int y) {
+    int change = 0;
+    int i, k;
+
+    for (i = x; i < x + 8; i = i + 2) {
+        for (k = y; k < y + 8; k = k + 2)
+            if (board[i][k] == 'B') change++;
+        for (k = y + 1; k < y + 8; k = k + 2)
+            if (board[i][k] == 'W') change++;
+    }
+
+    for (i = x + 1; i < x + 8; i = i + 2) {
+        for (k = y; k < y + 8; k = k + 2)
+            if (board[i][k] == 'W') change++;
+        for (k = y + 1; k < y + 8; k = k + 2)
+            if (board[i][k] == 'B') change++;
+    }
+
+    return change;
+}
+
+
+int main() {
+    int N, M;
+    cin >> N >> M;
+
+    vector<vector<char>> board(N, vector<char>(M));
+
+    // 보드 입력
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < M; j++)
+            cin >> board[i][j];
+
+    int minChange = 64; // 최대 변경 횟수는 64 (모든 칸을 다 바꿔야 하는 경우)
+
+    // 왼쪽 위 칸이 흰색인 경우를 고려
+    for (int i = 0; i <= N - 8; i++)
+        for (int j = 0; j <= M - 8; j++) {
+            int changeCount = white(board, i, j);
+            minChange = min(minChange, changeCount);
+        }
+
+    // 왼쪽 위 칸이 검은색인 경우를 고려
+    for (int i = 0; i <= N - 8; i++)
+        for (int j = 0; j <= M - 8; j++) {
+            int changeCount = black(board, i, j);
+            minChange = min(minChange, changeCount);
+        }
+
+    cout << minChange << endl;
+
+    return 0;
+}
