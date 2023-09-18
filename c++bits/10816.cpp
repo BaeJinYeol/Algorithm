6edb48e7caa5c546bc620277f565fa710a4eecc0
BaeJinYeol <qowlsduf4488@gmail.com>
2023-09-18 16:39:47 +0900

    23.09.18 10816 :tada:

--- c++bits/10816.cpp
+++ c++bits/10816.cpp
@@ -29,28 +29,28 @@ int main()
 //#include <iostream>
 //#include <vector>
 //#include <algorithm>
-//
+//using namespace std;
+// 
 //int main()
 //{
-//	std::ios::sync_with_stdio(false); std::cin.tie(nullptr);
+//	ios::sync_with_stdio(false);
+//  cin.tie(NULL);
 //
-//	int N, M, n;
-//	std::vector<int> v;
+//	int N, M, card, i;
+//	vector<int> v;
 //
-//	std::cin >> N;
-//	while (N--)
-//	{
-//		std::cin >> n;
-//		v.push_back(n);
+//	cin >> N;
+//	for (i = 0; i < N; i++) {
+//		cin >> card;
+//		v.push_back(card);
 //	}
 //
-//	std::sort(v.begin(), v.end());
+//	sort(v.begin(), v.end());
 //
-//	std::cin >> M;
-//	while (M--)
-//	{
-//		std::cin >> n;
-//		std::cout << std::upper_bound(v.begin(), v.end(), n)
-//			- std::lower_bound(v.begin(), v.end(), n) << ' ';
+//	cin >> M;
+//	for (i = 0; i < M; i++) {
+//		cin >> card;
+//		cout << upper_bound(v.begin(), v.end(), card)
+//			- lower_bound(v.begin(), v.end(), card) << ' ';
 //	}
 //}
