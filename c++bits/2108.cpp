d1232e91ed0f93a9ff86ae69180b2ac25502f9b7
BaeJinYeol <qowlsduf4488@gmail.com>
2023-09-25 20:49:06 +0900

    23.09.25 2108 실버3

---
+++ c++bits/2108.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+using namespace std;
+
+const int RANGE = 8001; // ¹üÀ§: -4000 ~ 4000
+
+int mode(vector<int> frequency) 
+{
+	int flag;
+	int max = 0;
+
+	for (int i = 0; i < RANGE; i++) {
+		if (frequency[i] > max) {
+			max = frequency[i];
+			flag = i;
+		}
+	}
+	for (int i = flag + 1; i < RANGE; i++) {
+		if (frequency[i] == max) {
+			flag = i;
+			break;
+		}
+	}
+	return flag - 4000;
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int n;
+	double sum = 0;
+	cin >> n;
+
+	vector<int> input(n);
+	vector<int> frequency(RANGE, 0);
+
+	for (int i = 0; i < n; i++) {
+		cin >> input[i];
+		sum += input[i];
+		frequency[input[i] + 4000]++;
+	}
+
+	sort(input.begin(), input.end());
+
+	// »ê¼úÆò±Õ, Áß¾Ó°ª, ÃÖºó°ª, ¹üÀ§
+	cout << int(round(sum / n)) << '\n';
+	cout << input[(n - 1) / 2] << '\n';
+	cout << mode(frequency) << '\n';
+	cout << input.back() - input.front() << '\n';
+
+	return 0;
+}
