b1313c39a47a1771bc1f06939331902ddda57246
BaeJinYeol <108410316+BaeJinYeol@users.noreply.github.com>
2023-10-08 22:19:22 +0900

    23.10.08 1463 실버3, 1로 만들기, DP

--- c++bits/1463.cpp
+++ c++bits/1463.cpp
@@ -15,14 +15,13 @@ int main()
 	memo[1] = 0;
 	memo[2] = 1;
 	memo[3] = 1;
-
+	
 	for (int i = 4; i <= n; i++) {
 		memo[i] = memo[i - 1] + 1;
 		if (i % 3 == 0) memo[i] = min(memo[i / 3] + 1, memo[i]);
 		if (i % 2 == 0) memo[i] = min(memo[i / 2] + 1, memo[i]);
 	}
-
-	cout << memo[n] << endl;
+	cout << memo[n] << '\n';
 
 	return 0;
 }
