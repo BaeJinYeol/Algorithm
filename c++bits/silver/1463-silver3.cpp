2daee722e95d94247dd9d5cfd26318d9be0f9990
BaeJinYeol <qowlsduf4488@gmail.com>
2023-11-03 16:24:01 +0900

    폴더 정리

---
+++ c++bits/silver/1463-silver3.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <utility>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int n;
+	cin >> n;
+	vector<int> memo(n+1, 0);
+
+	memo[1] = 0;
+	memo[2] = 1;
+	memo[3] = 1;
+	
+	for (int i = 4; i <= n; i++) {
+		memo[i] = memo[i - 1] + 1;
+		if (i % 3 == 0) memo[i] = min(memo[i / 3] + 1, memo[i]);
+		if (i % 2 == 0) memo[i] = min(memo[i / 2] + 1, memo[i]);
+	}
+	cout << memo[n] << '\n';
+
+	return 0;
+}
